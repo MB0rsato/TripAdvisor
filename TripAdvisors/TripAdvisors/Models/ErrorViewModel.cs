c5aaf09141650f938920bcbbff28b0b4237a31b0
MB0rsato <118889102+MB0rsato@users.noreply.github.com>
2024-03-25 10:28:20 +0100

    Project created

---
+++ TripAdvisors/TripAdvisors/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TripAdvisors.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

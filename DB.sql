6ccf184e4f41f10009135cccb5aa55f1273409df
MB0rsato <118889102+MB0rsato@users.noreply.github.com>
2024-03-25 10:19:13 +0100

    Correct DB

--- DB.sql
+++ DB.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Versione server:              10.4.32-MariaDB - mariadb.org binary distribution
 -- S.O. server:                  Win64
--- HeidiSQL Versione:            12.6.0.6765
+-- HeidiSQL Versione:            12.5.0.6677
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,17 +16,19 @@
 
 
 -- Dump della struttura del database tripadvisor
+DROP DATABASE IF EXISTS `tripadvisor`;
 CREATE DATABASE IF NOT EXISTS `tripadvisor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `tripadvisor`;
 
 -- Dump della struttura di tabella tripadvisor.commenti
+DROP TABLE IF EXISTS `commenti`;
 CREATE TABLE IF NOT EXISTS `commenti` (
   `id` int(11) NOT NULL,
   `testo` varchar(200) DEFAULT NULL,
-  `stato` varchar(50) DEFAULT NULL,
+  `stato` varchar(200) DEFAULT NULL,
   `voto` int(11) DEFAULT NULL,
-  `autore` varchar(50) DEFAULT NULL,
-  `eliminato` varchar(50) DEFAULT NULL,
+  `autore` varchar(200) DEFAULT NULL,
+  `eliminato` char(2) DEFAULT NULL,
   `idUscita` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `FK_commenti_uscite` (`idUscita`),
@@ -34,25 +36,25 @@ CREATE TABLE IF NOT EXISTS `commenti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dump dei dati della tabella tripadvisor.commenti: ~0 rows (circa)
+DELETE FROM `commenti`;
 
 -- Dump della struttura di tabella tripadvisor.uscite
+DROP TABLE IF EXISTS `uscite`;
 CREATE TABLE IF NOT EXISTS `uscite` (
   `id` int(11) NOT NULL DEFAULT 0,
   `data` date DEFAULT NULL,
-  `luogo` varchar(50) DEFAULT NULL,
-  `classi` varchar(50) DEFAULT NULL,
+  `luogo` varchar(200) DEFAULT NULL,
+  `classi` varchar(200) DEFAULT NULL,
   `durata` int(11) DEFAULT NULL,
-  `tipologia` varchar(50) DEFAULT NULL,
+  `tipologia` varchar(200) DEFAULT NULL,
   `prezzo` int(11) DEFAULT NULL,
-  `foto` varchar(50) DEFAULT NULL,
+  `foto` varchar(200) DEFAULT NULL,
   `descrizione` varchar(200) DEFAULT NULL,
-  `idcommento` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `FK_uscite_commenti` (`idcommento`),
-  CONSTRAINT `FK_uscite_commenti` FOREIGN KEY (`idcommento`) REFERENCES `commenti` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dump dei dati della tabella tripadvisor.uscite: ~0 rows (circa)
+DELETE FROM `uscite`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
